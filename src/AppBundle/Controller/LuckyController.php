7a97d41a11eeba74703e66fbaf67ccd13554f423
MaxiGaller <maxi.galler@googlemail.com>
2015-09-17 20:07:32 +0200

    LuckyController #4

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -14,19 +14,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class LuckyController extends Controller {
-
-    /**
-     * @Route("/lucky/number")
-     */
-    public function numberAction() {
-
-        $number = rand(0,100);
-
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
-    }
-
+    
     /**
      * @Route("/lucky/number/{count}")
      */
