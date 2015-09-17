4d54161f0478ce47e35e1c6f2adf1f28fd3d77a1
MaxiGaller <maxi.galler@googlemail.com>
2015-09-17 11:56:02 +0200

    LuckyController

---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MaxiUni
+ * Date: 17.09.15
+ * Time: 11:34
+ */
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController extends Controller {
+
+    /**
+     * @Route("/Lucky/Number")
+     */
+    public function numberAction() {
+        $number = rand(0,100);
+
+        return new Response( '<html><body>Lucky number: '.$number.'</body></html>' );
+    }
+}
